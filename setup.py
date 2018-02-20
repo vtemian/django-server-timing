3435b2b29e23a60cfb1dacbcc5fee3f3b5b127cb
Vlad Temian <vladtemian@gmail.com>
2018-02-20 23:17:25 +0200

    Update setup classifiers

--- setup.py
+++ setup.py
@@ -14,10 +14,16 @@ setup(
     include_package_data=True,
     install_requires=[],
     classifiers=[
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
